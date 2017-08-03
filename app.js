f4c113e2068062b1ee23a2ac64f5fee85c23f0e0
valentinCo <eersel-val@hotmail.com>
2017-08-03 09:41:57 +0200

    Impossible d'ecraser une case déja remplie

--- app.js
+++ app.js
@@ -1,16 +1,31 @@
 console.log("Morpion time");
-var i = 0;
+
+var count = 0;
+
+
 
 $(".case").click(function() { 
 
-	
-		if(i%2 === 0) {
+ // if(.1 && .2 && .3 === "<img>") {
+ // alert("Waouuuuuuuuuu");
+ //  }
+	if($(this).html() === "") {
+		if(count%2 === 0) {
+
 			($(this).html("<img src='Croix.png' width=50px height=50px>")); 
 		} else {
 
 			($(this).html("<img src='Cercle.png' width=50px height=50px>")); 
 		}
-		i++;
+
+		count++;
+	}
+
 	
 }); 
+
+$(".clear").click(function() {
+	$(".case").html("");
+	count = 0;
+});
 
