806c19147c420d6c5e0f46d98f776cd778c041e1
valentinCo <eersel-val@hotmail.com>
2017-08-03 16:16:32 +0200

    ajout des reset

--- app.js
+++ app.js
@@ -12,23 +12,36 @@ var joueur1 = prompt("Joueur 1:");
 var joueur2 = prompt("Joueur 2:");
 	$(".two").html(joueur2);
 
+function reset() {
+		$(".case").html("");
+		array = ["0", "0", "0", "0", "0", "0", "0", "0", "0"];
+}
+
 function condition(arg1, arg2) {
 	if(array[0] === arg1 && array[1] === arg1 && array[2] === arg1) {
 		alert("Joueur" +" "+ arg2+ " " + "gagne!");
+		reset();
 	} if (array[3] === arg1 && array[4] === arg1 && array[5] === arg1) {
 		alert("Joueur" +" "+ arg2+ " " + "gagne!");
+		reset();
 	} if (array[6] === arg1 && array[7] === arg1 && array[8] === arg1) {
 		alert("Joueur" +" "+ arg2+ " " + "gagne!");
+		reset();
 	} if (array[0] === arg1 && array[3] === arg1 && array[6] === arg1) {
 		alert("Joueur" +" "+ arg2+ " " + "gagne!");
+		reset();
 	} if (array[1] === arg1 && array[4] === arg1 && array[7] === arg1) {
 		alert("Joueur" +" "+ arg2+ " " + "gagne!");
+		reset();
 	} if (array[2] === arg1 && array[5] === arg1 && array[8] === arg1) {
 		alert("Joueur" +" "+ arg2+ " " + "gagne!"); 
+		reset();
 	} if (array[0] === arg1 && array[4] === arg1 && array[8] === arg1) {
 		alert("Joueur" +" "+ arg2+ " " + "gagne!"); 
+		reset();
 	} if (array[2] === arg1 && array[4] === arg1 && array[6] === arg1) {
 		alert("Joueur" +" "+ arg2+ " " + "gagne!") ;
+		reset();
 
 	}
 }
@@ -58,6 +71,7 @@ $(".case").click(function() {
 
  if(count === 9) {
  	alert("Match nul!!");
+ 	reset();
  }
 }); 
 
