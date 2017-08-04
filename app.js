a9a1fa58e5de7bdd1ccb105866b5e0b06762bba3
valentinCo <eersel-val@hotmail.com>
2017-08-04 15:24:54 +0200

    tour nbrGame

--- app.js
+++ app.js
@@ -16,27 +16,23 @@ var rond = 2;
 var score2 = 0;
 var joueur1;
 var joueur2;
-var nbrGame;
+var nbrGame = 0;
+var tour = 0;
 // var nbrGame = "";
 
 var array = ["0", "0", "0", "0", "0", "0", "0", "0", "0"];
 
-// var	nbrGame = prompt("Nombre de games ?")
-// 	$(".nbrPart").append(nbrGame);
-// var joueur1 = prompt("Joueur 1:");
-// $(".one").html(joueur1);
-// var joueur2 = prompt("Joueur 2:");
-// $(".two").html(joueur2);
 
 $( "#recup").click(function() {
 	
-		 joueur1 = $( ".j1" ).val();
-		$(".one").html(joueur1);
-		 joueur2 = $(".j2").val();
-		$(".two").html(joueur2);	
-		 nbrGame = $(".j3").val();
-		$(".nbrPart").html(nbrGame);
-	});
+	joueur1 = $( ".j1" ).val();
+	$(".one").html(joueur1);
+	joueur2 = $(".j2").val();
+	$(".two").html(joueur2);	
+	nbrGame = parseInt($(".j3").val());
+	$(".nbrPart").html(nbrGame);
+
+});
 
 
 
@@ -45,34 +41,43 @@ function reset() {
 	count = 0;
 	$(".case").html("");
 	array = ["0", "0", "0", "0", "0", "0", "0", "0", "0"];
+
 }
 
 function condition(arg1, arg2) {
 	if(array[0] === arg1 && array[1] === arg1 && array[2] === arg1) {
 		alert("Joueur" +" "+ arg2+ " " + "gagne!");
 		reset();
+		tour++;
 		
 	} if (array[3] === arg1 && array[4] === arg1 && array[5] === arg1) {
 		alert("Joueur" +" "+ arg2+ " " + "gagne!");
 		reset();
+		tour++;
 	} if (array[6] === arg1 && array[7] === arg1 && array[8] === arg1) {
 		alert("Joueur" +" "+ arg2+ " " + "gagne!");
 		reset();
+		tour++;
 	} if (array[0] === arg1 && array[3] === arg1 && array[6] === arg1) {
 		alert("Joueur" +" "+ arg2+ " " + "gagne!");
 		reset();
+		tour++;
 	} if (array[1] === arg1 && array[4] === arg1 && array[7] === arg1) {
 		alert("Joueur" +" "+ arg2+ " " + "gagne!");
 		reset();
+		tour++;
 	} if (array[2] === arg1 && array[5] === arg1 && array[8] === arg1) {
 		alert("Joueur" +" "+ arg2+ " " + "gagne!"); 
 		reset();
+		tour++;
 	} if (array[0] === arg1 && array[4] === arg1 && array[8] === arg1) {
 		alert("Joueur" +" "+ arg2+ " " + "gagne!"); 
 		reset();
+		tour;
 	} if (array[2] === arg1 && array[4] === arg1 && array[6] === arg1) {
 		alert("Joueur" +" "+ arg2+ " " + "gagne!") ;
 		reset();
+		tour++;
 
 
 	}
@@ -82,36 +87,45 @@ $(".case").click(function() {
 	var data = ($(this).data("col")) - 1;
  // console.log($(this).data("col"));
  // console.log(array[($(this).data("col"))- 1])
+ if(tour < nbrGame) {
+ 	if($(this).html() === "") {
+		if(count%2 === 0) {
+
+			($(this).html("<img src='Croix.png' width=50px height=50px>")); 
+			array[data] = croix;
+			condition(croix, joueur1);
+			console.log(count)
 
 
 
- if($(this).html() === "") {
- 	if(count%2 === 0) {
+		} else {
 
- 		($(this).html("<img src='Croix.png' width=50px height=50px>")); 
- 		array[data] = croix;
- 		condition(croix, joueur1);
- 		console.log(count)
- 		
- 		
+			($(this).html("<img src='Cercle.png' width=50px height=50px>")); 
+			array[data] = rond;
+			condition(rond, joueur2);
+			console.log(count)
+		}
+		console.log(array);
 
- 	} else {
+		count++;
+	}
 
- 		($(this).html("<img src='Cercle.png' width=50px height=50px>")); 
- 		array[data] = rond;
- 		condition(rond, joueur2);
- 		console.log(count)
- 	}
- 	console.log(array);
+	if(count === 9) {
+		alert("Match nul!!");
+		reset();
+		tour++;
+	}
+} if(tour === nbrGame){
 
- 	count++;
- }
+	alert("endgame");
+ 	tour = 0;
+	reset();
 
- if(count === 9) {
- 	alert("Match nul!!");
- 	reset();
- }
-}); 
+
+
+	console.log(tour);
+	console.log(nbrGame);
+}}); 
 
 $(".clear").click(function() {
 	$(".case").html("");
