16756ade0c4301e100b1c9bf35bcd31ce73eb4af
valentinCo <eersel-val@hotmail.com>
2017-08-04 16:30:28 +0200

    Morpion manque scoring

--- app.js
+++ app.js
@@ -1,13 +1,4 @@
-// $('#exampleModal').on('show.bs.modal', function (event) {
-//   var button = $(event.relatedTarget) // Button that triggered the modal
-//   var recipient = button.data('whatever') // Extract info from data-* attributes
-//   // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
-//   // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
-//   var modal = $(this)
-//   modal.find('.modal-title').text('New message to ' + recipient)
-//   modal.find('.modal-body input').val(recipient)
-// })
-$("#exampleModal").modal("show");
+$("#exampleModal").modal("show");  //Arriver du modal
 
 var count = 0;
 var vide = 0;
@@ -18,32 +9,27 @@ var joueur1;
 var joueur2;
 var nbrGame = 0;
 var tour = 0;
-// var nbrGame = "";
-
 var array = ["0", "0", "0", "0", "0", "0", "0", "0", "0"];
 
 
+//Insert joueurs nombres de tours
 $( "#recup").click(function() {
-	
 	joueur1 = $( ".j1" ).val();
 	$(".one").html(joueur1);
 	joueur2 = $(".j2").val();
 	$(".two").html(joueur2);	
 	nbrGame = parseInt($(".j3").val());
 	$(".nbrPart").html(nbrGame);
-
 });
 
-
-
-
+//reset de la partie dans les conditions de victoires
 function reset() {
 	count = 0;
 	$(".case").html("");
 	array = ["0", "0", "0", "0", "0", "0", "0", "0", "0"];
-
 }
 
+//condition de victoire
 function condition(arg1, arg2) {
 	if(array[0] === arg1 && array[1] === arg1 && array[2] === arg1) {
 		alert("Joueur" +" "+ arg2+ " " + "gagne!");
@@ -78,55 +64,47 @@ function condition(arg1, arg2) {
 		alert("Joueur" +" "+ arg2+ " " + "gagne!") ;
 		reset();
 		tour++;
-
-
 	}
 	
 }
+//click action
 $(".case").click(function() { 
 	var data = ($(this).data("col")) - 1;
  // console.log($(this).data("col"));
  // console.log(array[($(this).data("col"))- 1])
  if(tour < nbrGame) {
  	if($(this).html() === "") {
-		if(count%2 === 0) {
+ 		if(count%2 === 0) {
 
-			($(this).html("<img src='Croix.png' width=50px height=50px>")); 
-			array[data] = croix;
-			condition(croix, joueur1);
-			console.log(count)
+ 			($(this).html("<img src='Croix.png' width=50px height=50px>")); 
+ 			array[data] = croix;
+ 			condition(croix, joueur1);
+ 			console.log(count)
 
+ 		} else {
 
+ 			($(this).html("<img src='Cercle.png' width=50px height=50px>")); 
+ 			array[data] = rond;
+ 			condition(rond, joueur2);
+ 			console.log(count)
+ 		}
+ 		console.log(array);
 
-		} else {
+ 		count++;
+ 	}
 
-			($(this).html("<img src='Cercle.png' width=50px height=50px>")); 
-			array[data] = rond;
-			condition(rond, joueur2);
-			console.log(count)
-		}
-		console.log(array);
+ 	if(count === 9) {
+ 		alert("Match nul!!");
+ 		reset();
+ 		tour++;
+ 	}
+ } if(tour === nbrGame){
 
-		count++;
-	}
-
-	if(count === 9) {
-		alert("Match nul!!");
-		reset();
-		tour++;
-	}
-} if(tour === nbrGame){
-
-	alert("endgame");
+ 	alert("endgame");
  	tour = 0;
-	reset();
-
-
-
-	console.log(tour);
-	console.log(nbrGame);
-}}); 
-
+ 	reset();
+ }}); 
+//reset via bouton
 $(".clear").click(function() {
 	$(".case").html("");
 	// joueur1 = prompt("Joueur 1:");
